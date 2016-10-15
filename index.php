70529aab6a2315bd2d146598275d15bb626f1edc
Efe Özgür <efecuneytozgur@gmail.com>
2016-10-15 10:43:39 +0300

    arabul

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+	<meta charset="UTF-8">
+	<title>Döküman</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/stilim.css">
+	<link href="https://fonts.googleapis.com/css?family=Special+Elite" rel="stylesheet">
+	<script src="js/jquery-3.1.1.js"></script>
+
+	<script>
+		$(function(){
+			$("input[name=aratxt]").focus();
+		});
+	</script>	
+</head>
+<body>
+
+<div class="ortada">
+	
+		
+		<div align="center" class="butonlar">
+			<ul>
+			<li style="border:0; font-family: 'Special Elite', cursive; font-size:60px; color:lightblue;">ARABUL</li>
+			<li><input name="aratxt" class="form-control" type="text"></li>
+			<li><a href="#">Saçlarını yol getir</a></li>
+				
+			</ul>
+		</div>
+</div>
+</body>
+</html>
